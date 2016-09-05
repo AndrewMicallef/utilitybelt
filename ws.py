e227e1840eab29606cf06442689ac87f30499b0c
amicallef <amicallef@student.unimelb.edu.au>
2016-09-05 11:30:21 +1000

    fixes f - file fiasco

--- ws.py
+++ ws.py
@@ -16,8 +16,9 @@ class wsfile:
     def __init__(self, infile):
     
         self.file = h5py.File(infile, 'r')
+        f = self.file
                 
-        if 'VersionString' not in self.f['header']:
+        if 'VersionString' not in self.file['header']:
             #This is likeley the version 0.801 ws file!
             #Double check this against the new specification
             IsContinuous = 'header/IsContinuous'
@@ -26,7 +27,7 @@ class wsfile:
             clock = 'header/ClockAtExperimentStart'
         
         else:
-            version  = float(self.f['header/VersionString'].value[0])
+            version  = float(self.file['header/VersionString'].value[0])
         
             if version >= 0.915:
                 IsContinuous = 'header/AreSweepsContinuous'
@@ -71,10 +72,10 @@ class wsfile:
     
         analogDATA = {} #tmp list to hold the analog data
         
-        for group in self.f:
+        for group in self.file:
             if group != u'header':
                 for n in range(len(self.nameslist)):
-                    trace = self.f['%s/analogScans' %group].value[n].astype('float')
+                    trace = self.file['%s/analogScans' %group].value[n].astype('float')
                     trace = (np.array(trace) * (20.0 / 2**16))
             
                     try:  
@@ -94,8 +95,7 @@ class wsfile:
     def __exit__(self, ctx_type, ctx_value, ctx_traceback):
         self.file.close()
     
-    '''
-[Context Managers](http://docs.python-guide.org/en/latest/writing/structure/#context-managers)
+    '''[Context Managers](http://docs.python-guide.org/en/latest/writing/structure/#context-managers)
     -------
 
     A context manager is a Python object that provides extra contextual 
@@ -137,7 +137,6 @@ class wsfile:
     __enter__ method is called and whatever __enter__ returns is assigned to 
     f in the as f part of the statement. When the contents of the with block 
     is finished executing, the __exit__ method is then called.
-<!>
     '''
     
     def close(self):
